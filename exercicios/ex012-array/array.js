456a694fb6affc247658903f09bff23af09085f6
Gabriel Batista <gabriel.batts@outlook.com>
2022-02-06 11:14:03 -0300

    array e funções

---
+++ exercicios/ex012-array/array.js
@@ -0,0 +1,15 @@
+let vaga = [5,3,9,7,8,2]
+console.log(vaga)
+vaga.sort()
+console.log(vaga)
+let buscador = vaga.indexOf(5) // esse comando indexOf vai dizer em que posição ta o elemento 5 
+console.log(buscador)
+/*
+for (let c = 0; c < vaga.length; c++){
+    console.log(vaga[c])
+} 
+
+for (let c in vaga) {
+    console.log(vaga[c])
+}
+*/
---
+++ exercicios/ex013-funçao/funcao2.js
@@ -0,0 +1,4 @@
+let soma = function(n1,n2) {
+    return n1 + n2
+}
+console.log(soma(7,3))
