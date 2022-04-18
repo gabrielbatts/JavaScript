2812c152f69fdd4c4a788ebe7a6d0bc7101f12b2
Gabriel Batista <gabriel.batts@outlook.com>
2022-04-18 19:02:45 -0300

    validador de CPF terminado

--- JS_Udemy/desafio/desafio 06/cpf.js
+++ JS_Udemy/desafio/desafio 06/cpf.js
@@ -9,15 +9,37 @@ Object.defineProperty(this, 'cpfLimpo',{ // aqui  vai limpar o cpf tirando os po
 CpfValida.prototype.valida = function(){
     if(typeof this.cpfLimpo === 'undefined') return false
     if(this.cpfLimpo.length !== 11) return false
+    if (this.isSequencia()) return false
     const cpfParcial = this.cpfLimpo.slice(0, -2)
     const digito1 = this.criaDigito(cpfParcial)
-    return true
+    const digito2 = this.criaDigito(cpfParcial + digito1)
+    const novoCpf = cpfParcial + digito1 + digito2
+    return novoCpf === this.cpfLimpo
     
 }
 CpfValida.prototype.criaDigito = function(cpfParcial){
-    const cpfArray = Array.from(cpfParcial)
-    console.log(cpfArray)
+    const cpfArray = Array.from(cpfParcial) // aqui está transformando o cpf em array
+    let regressivo = cpfArray.length +1
+    let total = cpfArray.reduce((ac, valor) =>{
+        ac += (regressivo * Number(valor)) // aqui vai pegar cada numero do cpf e multiplicar em contagem regressiva começando do 10 ex:
+        /* 10  9  8  7  6  5  4  3  2
+            8  6  0  1  7  6  3  3  5     */
+        regressivo --
+        return ac
+    },0)
+    const digito = 11 - (total % 11)
+    return digito > 9 ? '0' : String(digito) // se digito formaior que 9 retorne  se ñ retorne digito   
+}
+CpfValida.prototype.isSequencia = function(){
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+    return sequencia === this.cpfLimpo
 }
 const cpf = new CpfValida('860.176.335-97')
-console.log(cpf.cpfLimpo)
 console.log(cpf.valida())
+
+if (cpf.valida()){
+    console.log('CPF Válido')
+}
+else{
+    console.log('CPF Inválido')
+}
